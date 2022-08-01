fa40f745a508b1e5f0afdff1c7c5e41b300483cb
Denis <81900783+MoboGod@users.noreply.github.com>
2022-08-01 11:13:15 +0300

    Add files via upload

---
+++ Expense-Tracker/Expense-Tracker/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+	public class Transaction
+	{
+		[Key]
+        public int TransactionId { get; set; }
+
+        //CategoryId
+        public int  CategoryId { get; set; }
+
+        public int  Amount { get; set; }
+
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
+
